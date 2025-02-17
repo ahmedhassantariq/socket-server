be302a18e29bfc126bd9588129b1b7ed6e6a23cc
ahmedhassantariq <atariq344@gmail.com>
2025-02-17 22:47:13 +0500

    updated

--- server.js
+++ server.js
@@ -1,19 +1,67 @@
 const httpServer = require('http').createServer()
-const io = require('socket.io')(httpServer)
+const io = require('socket.io')(httpServer,{
+  maxHttpBufferSize: 1e15
+})
 
 // console.log(socketIO.engine.clientCount());
 
+var users = {};
+
+
+
 io.on('connection', socket => {
 
-  console.log('Connected...', socket.id);
-  console.log(io.of("/").sockets.size);
+  const connectID = socket['handshake']['auth']['userID'];
+  if(connectID==null||connectID==""){
+    console.log("Disconnecting", socket.id);
+    socket.disconnect();
+    console.log(io.of("/").sockets.size);
+
+  }else{
+    console.log("UserID: ", socket['handshake']['auth']['userID']);
+    console.log('Connected...', socket.id);
+    console.log(io.of("/").sockets.size);
+    users[connectID] = socket.id;
+    console.log(users);
+
+  }
+
   socket.on("message", (msg) => {
-    io.to(msg['receiverID']).emit("message", msg);
+    const receiverID = msg['receiverID'];
+    const receiverSocketID = users[receiverID];
+    console.log(msg);
+    if(receiverSocketID!=null&&receiverSocketID!=''){
+      io.to(receiverSocketID).emit("message", msg);
+      console.log("Sending to:", receiverSocketID);
+    }
+  
+  });
+
+  socket.on("data", (msg) => {
+    const receiverID = msg['receiverID'];
+    const receiverSocketID = users[receiverID];
+    console.log(msg);
+    if(receiverSocketID!=null&&receiverSocketID!=''){
+      io.to(receiverSocketID).emit("data", msg);
+      console.log("Sending to:", receiverSocketID);
+    }
+  
   });
 
 socket.on('disconnect', function () {
     console.log(io.of("/").sockets.size);
     console.log('Disconnected...', socket.id);
+
+    for(const [userID, id] of Object.entries(users)){
+      if(id==socket.id){
+        delete users[userID];
+        console.log("User Removed", userID);
+        console.log(users);
+        break;
+      }
+    }
+
+
   })
 
   socket.on('error', function (err) {
